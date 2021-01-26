70ef2711b39377784cec8848281cd18a1413be76
heng <772330747@qq.com>
2021-01-26 10:38:25 +0800

    创建模型

--- bookmanager/bookmanager/settings.py
+++ bookmanager/bookmanager/settings.py
@@ -37,11 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    #fanganyi
+    #fangani
     'book'
     #fangan2
     # 'book.apps.BookConfig'
-    
+
 ]
 
 MIDDLEWARE = [
